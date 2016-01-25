2d9b6cadad85781fd38d2d3a6a6e5d18e606dc4e
Sietse Ringers <mail@sietseringers.net>
2016-01-25 21:47:24 +0100

    Add missing constructor to IdentityProviderRequest

--- src/main/java/org/irmacard/api/common/IdentityProviderRequest.java
+++ src/main/java/org/irmacard/api/common/IdentityProviderRequest.java
@@ -1,3 +1,7 @@
 package org.irmacard.api.common;
 
-public class IdentityProviderRequest extends ClientRequest<IssuingRequest> {}
+public class IdentityProviderRequest extends ClientRequest<IssuingRequest> {
+	public IdentityProviderRequest(String data, IssuingRequest request, int timeout) {
+		super(data, request, timeout);
+	}
+}
