d1fc90c84dde3b84359cedf0a14b91267a3aab4e
Sietse Ringers <mail@sietseringers.net>
2016-04-19 14:22:59 +0200

    Update and rearrange ApiErrors

--- src/main/java/org/irmacard/api/common/exceptions/ApiError.java
+++ src/main/java/org/irmacard/api/common/exceptions/ApiError.java
@@ -6,28 +6,30 @@ package org.irmacard.api.common.exceptions;
  */
 @SuppressWarnings("unused")
 public enum ApiError {
-	// IdP errors
+	// IdP-specific errors
 	MALFORMED_ISSUER_REQUEST(400, "Malformed issuer request"),
-	ATTRIBUTES_WRONG(400, "Specified attribute(s) do not belong to this credential type"),
 	INVALID_TIMESTAMP(400, "Timestamp was not an epoch boundary"),
-	JWT_INVALID(401, "JSON web token did not verify"),
-	JWT_TOO_OLD(401, "JSON web token was too old"),
-	UNAUTHORIZED(403, "You are not authorized to issue this credential"),
 	ISSUING_DISABLED(403, "This server does not support issuing"),
 
-	// SP errors
+	// SP-specific errors
 	MALFORMED_VERIFIER_REQUEST(400, "Malformed verification request"),
-
-	// SP, IdP, or token errors
-	SESSION_UNKNOWN(400, "Unknown or expired session"),
-	SESSION_TOKEN_MALFORMED(400, "Malformed session token"),
-	MALFORMED_INPUT(400, "Input could not be parsed"),
 
 	// Token errors
 	INVALID_PROOFS(400, "Invalid secret key commitments and/or disclosure proofs"),
 	ATTRIBUTES_MISSING(400, "Not all requested-for attributes were present"),
 	ATTRIBUTES_EXPIRED(400, "Disclosed attributes were expired"),
 	UNEXPECTED_REQUEST(403, "Unexpected request in this state"),
+
+	// IdP or SP errors
+	JWT_INVALID(401, "JSON web token did not verify"),
+	JWT_TOO_OLD(401, "JSON web token was too old"),
+	UNAUTHORIZED(403, "You are not authorized to issue or verify this attribute"),
+	ATTRIBUTES_WRONG(400, "Specified attribute(s) do not belong to this credential type"),
+
+	// SP, IdP, or token errors
+	SESSION_UNKNOWN(400, "Unknown or expired session"),
+	SESSION_TOKEN_MALFORMED(400, "Malformed session token"),
+	MALFORMED_INPUT(400, "Input could not be parsed"),
 
 	// Our errors
 	CANNOT_ISSUE(500, "Cannot issue this credential"),
