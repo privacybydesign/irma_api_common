cda0c830871e73aeff7637582c8401f66806d92b
Sietse Ringers <mail@sietseringers.net>
2016-12-01 21:02:02 +0100

    Support sending public keys in a JwtSessionRequest

--- src/main/java/org/irmacard/api/common/JwtSessionRequest.java
+++ src/main/java/org/irmacard/api/common/JwtSessionRequest.java
@@ -1,16 +1,25 @@
 package org.irmacard.api.common;
 
+import org.irmacard.credentials.info.IssuerIdentifier;
+
 import java.math.BigInteger;
+import java.util.HashMap;
 
 public class JwtSessionRequest {
 	private String jwt;
 	private BigInteger nonce;
 	private BigInteger context;
+	private HashMap<IssuerIdentifier, Integer> keys;
 
 	public JwtSessionRequest(String jwt, BigInteger nonce, BigInteger context) {
 		this.jwt = jwt;
 		this.nonce = nonce;
 		this.context = context;
+	}
+
+	public JwtSessionRequest(String jwt, BigInteger nonce, BigInteger context, HashMap<IssuerIdentifier, Integer> keys) {
+		this(jwt, nonce, context);
+		this.keys = keys;
 	}
 
 	public String getJwt() {
@@ -23,5 +32,9 @@ public class JwtSessionRequest {
 
 	public BigInteger getContext() {
 		return context;
+	}
+
+	public HashMap<IssuerIdentifier, Integer> getPublicKeys() {
+		return keys;
 	}
 }
