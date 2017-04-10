ef5a61024619088fd1509f0d1db512b209e66327
Sietse Ringers <mail@sietseringers.net>
2017-04-10 17:33:42 +0200

    Resolve code duplication in ApiClient

--- src/main/java/org/irmacard/api/common/ApiClient.java
+++ src/main/java/org/irmacard/api/common/ApiClient.java
@@ -1,16 +1,16 @@
 package org.irmacard.api.common;
 
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.apache.commons.codec.binary.Base64;
 import org.irmacard.api.common.disclosure.DisclosureProofRequest;
 import org.irmacard.api.common.disclosure.ServiceProviderRequest;
+import org.irmacard.api.common.issuing.IdentityProviderRequest;
+import org.irmacard.api.common.issuing.IssuingRequest;
 import org.irmacard.api.common.util.GsonUtil;
 import org.irmacard.credentials.Attributes;
 import org.irmacard.credentials.info.CredentialIdentifier;
-import org.apache.commons.codec.binary.Base64;
-
-import org.irmacard.api.common.issuing.IssuingRequest;
-import org.irmacard.api.common.issuing.IdentityProviderRequest;
 
 import java.security.PrivateKey;
 import java.util.ArrayList;
@@ -24,50 +24,45 @@ public class ApiClient {
 
     public static String getIssuingJWT(HashMap<CredentialIdentifier, HashMap<String, String>> credentialList, String keyID, String iss, boolean shouldSign, SignatureAlgorithm sigAlg, PrivateKey privKey) {
         return shouldSign ?
-                getSignedIssuingJWT(credentialList,keyID,iss,sigAlg, privKey) :
-                getUnsignedIssuingJWT(credentialList,iss);
+                getSignedIssuingJWT(credentialList, keyID, iss, sigAlg, privKey) :
+                getUnsignedIssuingJWT(credentialList, iss);
+    }
+
+    public static String getSignedIssuingJWT(HashMap<CredentialIdentifier, HashMap<String, String>> credentialList, String iss, SignatureAlgorithm sigAlg, PrivateKey privKey) {
+        return getSignedIssuingJWT(credentialList, null, iss, sigAlg, privKey);
     }
 
     public static String getSignedIssuingJWT(HashMap<CredentialIdentifier, HashMap<String, String>> credentialList, String keyID, String iss, SignatureAlgorithm sigAlg, PrivateKey privKey) {
-        return Jwts.builder()
-                .setHeaderParam("kid", keyID)
-                .setPayload(getJwtClaims(credentialList,iss))
-                .signWith(sigAlg,
-                        privKey)
-                .compact();
+        IdentityProviderRequest ipRequest = getIdentityProviderRequest(credentialList);
+        return buildJwt(keyID, sigAlg, privKey, getJwtClaims(ipRequest, "iprequest", "issue_request", iss));
     }
 
     public static String getUnsignedIssuingJWT(HashMap<CredentialIdentifier, HashMap<String, String>> credentialList, String iss) {
+        IdentityProviderRequest ipRequest = getIdentityProviderRequest(credentialList);
         String header = encodeBase64("{\"typ\":\"JWT\",\"alg\":\"none\"}");
-        String claims = encodeBase64(getJwtClaims(credentialList, iss));
-        return header + "." + claims + ".";
+        String claims = encodeBase64(getJwtClaims(ipRequest, "iprequest", "issue_request", iss));
+        return header + "." + claims;
+    }
+
+    public static String getDisclosureJWT(AttributeDisjunctionList list, String iss, SignatureAlgorithm sigAlg, PrivateKey privKey) {
+        return getDisclosureJWT(list, null, iss, sigAlg, privKey);
     }
 
     public static String getDisclosureJWT(AttributeDisjunctionList list, String keyID, String iss, SignatureAlgorithm sigAlg, PrivateKey privKey) {
         DisclosureProofRequest request = new DisclosureProofRequest(null, null, list);
         ServiceProviderRequest spRequest = new ServiceProviderRequest("", request, 120);
-
-        return Jwts.builder()
-                .setHeaderParam("kid", keyID)
-                .setPayload(getJwtClaims(spRequest, "sprequest", "verification_request",iss))
-                .signWith(sigAlg,
-                        privKey)
-                .compact();
+        return buildJwt(keyID, sigAlg, privKey, getJwtClaims(spRequest, "sprequest", "verification_request", iss));
     }
 
+    private static String buildJwt(String keyID, SignatureAlgorithm sigAlg, PrivateKey privKey, String request) {
+        JwtBuilder builder = Jwts.builder();
+        if (keyID != null)
+            builder.setHeaderParam("kid", keyID);
 
-    /**
-     * Serialize the credentials to be issued to the body (claims) of a JWT token
-     */
-    //TODO: remove following code duplication:
-    private static String getJwtClaims(HashMap<CredentialIdentifier, HashMap<String, String>> credentialList, String iss) {
-        HashMap<String, Object> claims = new HashMap<>(4);
-        claims.put("iprequest", getIdentityProviderRequest(credentialList));
-        claims.put("iat", System.currentTimeMillis()/1000);
-        claims.put("iss", iss);
-        claims.put("sub", "issue_request");
-
-        return GsonUtil.getGson().toJson(claims);
+        return builder
+                .setPayload(request)
+                .signWith(sigAlg, privKey)
+                .compact();
     }
 
     /**
@@ -87,9 +82,9 @@ public class ApiClient {
     }
 
 
-        /**
-         * Convert the credentials to be issued to an {@link IdentityProviderRequest} for the API server
-         */
+    /**
+     * Convert the credentials to be issued to an {@link IdentityProviderRequest} for the API server
+     */
     public static IdentityProviderRequest getIdentityProviderRequest(HashMap<CredentialIdentifier, HashMap<String, String>> credentialList) {
         // Calculate expiry date: 6 months from now
         Calendar calendar = Calendar.getInstance();
